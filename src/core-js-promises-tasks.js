f8222f36c183a6172ad2c76792b46b52d77a0fac
AishSI <aishsi@gmail.com>
2024-04-08 13:48:15 +0700

    feat: add solution 'queuPromises' task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -138,8 +138,22 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  const result = [];
+
+  function myPromiseAll(index = 0) {
+    if (index === promises.length) {
+      return Promise.resolve(result.join(''));
+    }
+
+    return promises[index].then((value) => {
+      result.push(value);
+
+      return myPromiseAll(index + 1);
+    });
+  }
+
+  return myPromiseAll();
 }
 
 module.exports = {
